b8d0003220b70a13d565fa8a7219a0da3179da50
Jose Carlos Canova <jose.canova@ext.valid.com>
2018-11-07 11:06:12 -0200

    Remocao import

--- src/main/java/net/superbid/java/backend/service/PostRepository.java
+++ src/main/java/net/superbid/java/backend/service/PostRepository.java
@@ -1,8 +1,5 @@
 package net.superbid.java.backend.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 
 import net.superbid.java.backend.model.Post;
