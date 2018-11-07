5af553d98846685c6dce04c2585eb817a030cd49
Jose Carlos Canova <jose.canova@ext.valid.com>
2018-11-07 13:36:58 -0200

    Removed mutator over Pojo class

--- src/main/java/net/superbid/java/backend/model/Post.java
+++ src/main/java/net/superbid/java/backend/model/Post.java
@@ -48,12 +48,9 @@ public class Post implements Base<Long>{
 
 	public Post() {}
 	
+	@JsonProperty(access=JsonProperty.Access.READ_ONLY)
 	public Long getId() {
 		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getContent() {
